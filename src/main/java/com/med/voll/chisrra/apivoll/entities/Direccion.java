cfc386f3a500fdadff3839da59a7a3fdb7ad9f34
Chisrra <75094536+Chisrra@users.noreply.github.com>
2023-09-17 15:05:37 -0600

    Connection with SQL DB and entity Medico

---
+++ src/main/java/com/med/voll/chisrra/apivoll/entities/Direccion.java
@@ -0,0 +1,18 @@
+package com.med.voll.chisrra.apivoll.entities;
+
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Embeddable
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Direccion {
+    private String calle;
+    private int numero;
+    private String complemento;
+    private String distrito;
+    private String ciudad;
+}
---
+++ src/main/java/com/med/voll/chisrra/apivoll/entities/Medico.java
@@ -0,0 +1,27 @@
+package com.med.voll.chisrra.apivoll.entities;
+
+import com.med.voll.chisrra.apivoll.medico.Especialidad;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity(name = "Medico")
+@Table(name = "medicos")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@EqualsAndHashCode(of = "id")
+public class Medico {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nombre;
+    private String email;
+    private String documento;
+    @Enumerated(EnumType.STRING)
+    private Especialidad especialidad;
+    @Embedded
+    private Direccion direccion;
+}
